049f43c3e46e21bf75a89a94808abf4f5130b599
allsers <162462688+allsers@users.noreply.github.com>
2024-10-20 17:33:21 +0200

    Updates

---
+++ _app/immutable/chunks/paths.BlH6KBBl.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_13q774m)==null?void 0:s.base)??"/ufc_site";var e;const a=((e=globalThis.__sveltekit_13q774m)==null?void 0:e.assets)??t;export{a,t as b};
--- _app/immutable/entry/start.BArRYrIm.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.BOR9yc_C.js";export{t as start};
